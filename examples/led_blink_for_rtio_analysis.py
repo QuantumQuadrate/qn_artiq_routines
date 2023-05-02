4b659300e0b93b16e72e726a55ef231a0e5a0316
prhuft <gothrowayo@gmail.com>
2023-05-02 14:08:20 -0500

    slightly more complicated example

--- examples/led_blink_for_rtio_analysis.py
+++ examples/led_blink_for_rtio_analysis.py
@@ -5,6 +5,9 @@ See here https://forum.m-labs.hk/d/188-analyzing-rtio-slack-with-gtkwave
 To analyze the rtio slack run "artiq_coreanalyzer -w yourfilename.vcd" in
 an Anaconda prompt in the artiq-master directory. Then open the waveform
 with "gtkwave yourfilename.vcd" (assumes you added gtkwave to path).
+
+
+Only uncomment the run function that you want to analyze.
 """
 
 from artiq.experiment import *
@@ -19,15 +22,15 @@ class BlinkForever(EnvExperiment):
     # underflow occurs because events are being added to the scheduler slower than they
     # are being dispatched, so at some point we're scheduling a device to be run at a
     # time stamp that already happened.
-    @kernel
-    def run(self):
-        self.core.reset()
-        while True:
-            self.led0.pulse(.1*us)
-            delay(.1*us)
+    # @kernel
+    # def run(self):
+    #     self.core.reset()
+    #     while True:
+    #         self.led0.pulse(.1*us)
+    #         delay(.1*us)
 
     # this seems to go on "forever" with no problem. terminate it in the dashboard.
-    # now, the events are being submitted slower than they are actually being run
+    # now, the events are being submitted more slowly than they are actually being run
     # and the slack remains positive (and actually increases). Around 48 ms the
     # slack wraps around, and this continues to happen, so this will go until we
     # stop it or the hardware gives out.
@@ -37,5 +40,20 @@ class BlinkForever(EnvExperiment):
     #     while True:
     #         self.led0.pulse(1 * us)
     #         delay(1 * us)
+
+    # more complicated example
+    @kernel
+    def run(self):
+        self.core.reset()
+        for i in range(10):
+            for i in range(10):
+                self.led0.pulse(10 * ns)
+                delay(10 * ns)
+            # delay(50 * us)
+            self.core.reset() # in lieu of a delay, just cheat and reset the clock?
+            for i in range(10):
+                self.led0.pulse(10 * ns)
+                delay(10 * ns)
+        print("blink example done.")
 
     # todo: add an overflow example
