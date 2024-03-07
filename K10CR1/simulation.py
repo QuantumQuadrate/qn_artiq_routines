1e9ac43e1cabc49ad1a2b36394333004e5a68526
Jake Uribe <jeuribe@wisc.edu>
2024-03-07 12:19:31 -0600

    changes

--- K10CR1/simulation.py
+++ K10CR1/simulation.py
@@ -3,6 +3,8 @@ from scipy.optimize import minimize, curve_fit
 from scipy.signal import argrelmax
 import matplotlib.pyplot as plt
 from time import sleep
+from rotator_feedback import RotatorFeedbackChannel
+
 
 sin = np.sin
 cos = np.cos
@@ -69,7 +71,7 @@ def hwp(fast_axis_angle = 90, piecewise = False):
     else:
         return matrix[0][0], matrix[0][1], matrix[1][0], matrix[1][1]
 
-def measure(q_ang = 45, h_ang = 100, theta = 3*pi/4, phi = pi/3, eta = pi/4, E = 1,  theta_h = 0, theta_q = 0):
+def measure(q_ang = 45, h_ang = 100, theta = 3*pi/4, phi = pi/3, eta = pi/4, E = 1,  theta_h = 0, theta_q = 0, a = 0):
     qwp00, qwp01, qwp10, qwp11 = qwp(fast_axis_angle=q_ang-theta_q, piecewise=True)
     hwp00, hwp01, hwp10, hwp11 = hwp(fast_axis_angle=h_ang-theta_h, piecewise=True)
 
@@ -85,41 +87,48 @@ def measure(q_ang = 45, h_ang = 100, theta = 3*pi/4, phi = pi/3, eta = pi/4, E =
     state_lp2 = ar10 * A_pp + ar11 * B_pp
     value = np.sqrt(abs(state_lp1)**2)
 
-    return value
+    return value + a
 
 
 def move_and_measure(theta, eta, phi, range, steps, a = 0, b = 0, theta_h = 0, theta_q = 0, dry_run = True,
                      r_feedback = None, E = None):
     q_ang = np.random.uniform(low = -range/2,high = range/2,size =steps) + a - theta_q
     h_ang = np.random.uniform(low = -range/2, high = range/2, size = steps) + b - theta_h
-    q_ang.sort()
-    h_ang.sort()
+    #q_ang.sort()
+    #h_ang.sort()
     measurements = []
     if dry_run or r_feedback is None:
         for q,h in zip(q_ang, h_ang):
             measurements.append(np.sum(measure(q_ang=q, h_ang=h, theta = theta, phi=phi,
                                                eta = eta, E = E)))
-    """else:
+    else:
         for q, h in zip(q_ang, h_ang):
             r_feedback.stage[1].move_to(q)
             r_feedback.stage[0].move_to(h)
-            r_feedback.stage[0].wait_for_stop()
+            while r_feedback.is_moving():
+
+
             r_feedback.stage[1].wait_for_stop()
+            r_feedback.stage[0].wait_for_stop()
+
             measurement = r_feedback.measure()
             measurements.append(measurement)
             sleep(1)
-            print(measurement)"""
+            print(measurement)
 
     return q_ang, h_ang, measurements
 
+def constraint(x, args):
+    return (x[3] - x[6] - args)
 
 def objective_func(x, args):
     theta = x[0]
     eta = x[1]
     phi = x[2]
     E = x[3]
-    phase_q = 0
-    phase_h = 0
+    phase_q = x[4]
+    phase_h = x[5]
+    a = x[6]
     q_ang = args[0]
     h_ang = args[1]
     measurements = args[2]
@@ -128,7 +137,7 @@ def objective_func(x, args):
     for q, h, measurement in zip(q_ang, h_ang, measurements):
 
         error += (np.sum(measure(q_ang=q, h_ang =h, theta=theta, phi = phi, eta = eta, theta_q=phase_q,
-                                 theta_h=phase_h, E=E)
+                                 theta_h=phase_h, E=E, a = a)
                          -measurement)*1000)**2
     return error*1000
 
@@ -144,16 +153,29 @@ def gen_secrets(default = False):
         eta = rand()*pi
         phi = rand()*pi
         E = rand()*2
+
         return theta, eta, phi, E
 
-range_val = 90
-steps = 30
+
+
+
+rotor_channel = RotatorFeedbackChannel(ch_name="Dev1/ai0", rotator_sn=["55105674", "55000741"], dry_run=False)
+rotor_channel.stage[0].stop()
+rotor_channel.stage[1].stop()
+#rotor_channel.stage[0].move_to(0)
+#rotor_channel.stage[1].move_to(0)
+rotor_channel.stage[0].wait_for_stop()
+rotor_channel.stage[1].wait_for_stop()
+
+
+range_val = 180
+steps = 10
 theta_0, eta_0, phi_0, E_0 = gen_secrets(default=False)
 
 q_ang, h_ang, measurements = move_and_measure(phi=phi_0, theta = theta_0, eta = eta_0, range = range_val,
-                                              steps = steps, r_feedback = None, dry_run=True, E = E_0, a=0, b= 0)
+                                              steps = steps, r_feedback = rotor_channel, dry_run=False, E = E_0, a=0, b= 0)
 
-initial_guess = [rand()*pi, rand()*pi, rand()*pi, rand()*2]
+x0 = [rand()*pi, rand()*pi, rand()*pi, max(measurements)*1.2, 0, 0, 0]
 
 args = [[],[],[]]
 
@@ -161,14 +183,18 @@ args[0].append(q_ang)
 args[1].append(h_ang)
 args[2].append(measurements)
 #print(args)
+cons = ({'type': 'ineq',
+       'fun': constraint,
+       'args': (max(args[2]),)
+       })
 
-bounds = [(0, pi), (0, pi), (0, pi), (max(measurements), np.inf)]
-result = minimize(objective_func, x0 = initial_guess, args=args, bounds=bounds, method='trust-constr', tol = 1e-15,)
+bounds = [(0, pi), (0, pi), (0, pi), (max(measurements)-min(measurements), np.inf), (0, pi), (0, pi), (-0.5, min(measurements))]
+result = minimize(objective_func, x0 = x0, args=args, bounds=bounds, method='trust-constr', tol = 1e-15, constraints = cons)
 x = result.x
-theta, eta, phi, E = x
+theta, eta, phi, E, p_q, p_h, a = x
 
-q = np.linspace(-range_val,range_val, 30)
-h = np.linspace(-range_val,range_val, 30)
+q = np.linspace(-range_val/2,range_val/2, 30)
+h = np.linspace(-range_val/2,range_val/2, 30)
 X, Y = np.meshgrid(q,h)
 print(E-E_0)
 print(theta-theta_0)
@@ -177,19 +203,23 @@ print(phi-phi_0)
 
 
 Z = measure(q_ang=X, h_ang=Y, theta = theta_0, eta=eta_0, phi=phi_0, theta_q = 0, theta_h = 0, E = E_0)
-Z1 = measure(q_ang=X, h_ang=Y, theta = theta, eta=eta, phi=phi, theta_q = 0, theta_h = 0, E = E)
+Z1 = measure(q_ang=X, h_ang=Y, theta = theta, eta=eta, phi=phi, theta_q = p_q, theta_h = p_h, E = E, a = a)
 
 fig = plt.figure()
 #ax = plt.axes(projection='3d')
 ax = fig.add_subplot(projection ="3d")
 #ax.scatter(q_ang, h_ang, measurements, s = 30, marker="*")
-ax.scatter(X, Y, Z, marker=".", s=3)
+#ax.scatter(X, Y, Z, marker=".", s=3)
 ax.scatter(X, Y, Z1, marker = ".", s=5)
+ax.scatter(q_ang, h_ang, measurements)
 c = argrelmax(Z1, order = 100)
-#maxX, maxY, maxZ = X[c], Y[c], Z1[c]
-#measurementMaxIndex = np.where(Z1 == (max(Z1[c])))
-#print(maxX[measurementMaxIndex[0]])
-#print((maxY[measurementMaxIndex[0]]))
+maxX, maxY, maxZ = X[c], Y[c], Z1[c]
+measurementMaxIndex = np.where(Z1 == (max(Z1[c])))
+maxVals = np.sum(maxX[measurementMaxIndex[0]]), np.sum(maxY[measurementMaxIndex[0]])
+rotor_channel.move_to(degrees=maxVals[0], rotor_num=0)
+rotor_channel.move_to(degrees=maxVals[1], rotor_num=1)
+rotor_channel.wait_stop()
+
 
 #q_ang, h_ang, measurements = move_and_measure(phi_x=phi_x, phi_y = phi_y, rand_axis=rand_axis, range = range_val, steps = 10)
 
@@ -199,3 +229,4 @@ ax.set_ylabel('y')
 ax.set_zlabel('z')
 ax.set_title(' ')
 plt.show()
+rotor_channel.close()
