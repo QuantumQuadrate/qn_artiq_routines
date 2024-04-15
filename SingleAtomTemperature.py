29700cb8bd25a3ccb31bb387982ff39ae62da8aa
prhuft <gothrowayo@gmail.com>
2024-04-15 16:09:13 -0500

    removed arguments which are already in ExperimentVariables, renamed the release time variable for clarity, use atom_loading_experiment for nearer-to-optimal code reuse

--- SingleAtomTemperature.py
+++ SingleAtomTemperature.py
@@ -9,7 +9,7 @@ import numpy as np
 from datetime import datetime as dt
 
 from utilities.BaseExperiment import BaseExperiment
-from subroutines.experiment_functions import load_MOT_and_FORT
+from subroutines.experiment_functions import atom_loading_experiment
 
 
 class SingleAtomTemperature(EnvExperiment):
@@ -22,21 +22,19 @@ class SingleAtomTemperature(EnvExperiment):
         self.base.build()
 
         # this is an argument for using a scan package, maybe
-        self.scan_datasets = ["t_delay_between_shots_sequence"]
+        self.scan_datasets = ["t_FORT_drop_sequence"]
         try:
             for dataset in self.scan_datasets:
                 value = self.get_dataset(dataset)
                 self.setattr_argument(dataset, StringValue(value))
         except KeyError as e:
             print(e)
-            self.setattr_argument("t_delay_between_shots_sequence", StringValue(
+            self.setattr_argument("t_FORT_drop_sequence", StringValue(
                 'np.array([1.0, 10.0, 50.0, 100.])*us'))
 
-        self.setattr_argument("n_measurements", NumberValue(10, ndecimals=0, step=1))
         self.setattr_argument("no_first_shot", BooleanValue(False))
         self.setattr_argument("do_PGC_in_MOT", BooleanValue(False))
         self.setattr_argument("bins", NumberValue(50, ndecimals=0, step=1), "Histogram setup (set bins=0 for auto)")
-        self.setattr_argument("enable_laser_feedback", BooleanValue(default=True),"Laser power stabilization")
 
         self.base.set_datasets_from_gui_args()
         print("build - done")
@@ -55,8 +53,8 @@ class SingleAtomTemperature(EnvExperiment):
         self.sampler_buffer = np.full(8, 0.0)
         self.cooling_volts_ch = 7
 
-        self.t_delay_between_shots_list = eval(self.t_delay_between_shots_sequence)
-        self.n_iterations = len(self.t_delay_between_shots_list)
+        self.t_FORT_drop_list = eval(self.t_FORT_drop_sequence)
+        self.n_iterations = len(self.t_FORT_drop_list)
 
         print("prepare - done")
 
@@ -79,7 +77,6 @@ class SingleAtomTemperature(EnvExperiment):
         # todo: these are going to be regularly used, so put these in the base experiment
         self.set_dataset("photocounts", [0], broadcast=True)
         self.set_dataset("photocounts2", [0], broadcast=True)
-
         self.set_dataset("photocount_bins", [self.bins], broadcast=True)
 
 
@@ -103,66 +100,69 @@ class SingleAtomTemperature(EnvExperiment):
         counts2 = 0
 
         iteration = 0
-        for t_delay_between_shots in self.t_delay_between_shots_list:
+        for t_FORT_drop in self.t_FORT_drop_list:
 
             # these are the datasets for plotting only, an we restart them each iteration
             self.set_dataset("photocounts_current_iteration", [0], broadcast=True)
             self.set_dataset("photocounts2_current_iteration", [0], broadcast=True)
 
+            self.t_FORT_drop = t_FORT_drop
+            atom_loading_experiment(self)
+
             # loop the experiment sequence
-            for measurement in range(self.n_measurements):
-
-                if self.enable_laser_feedback:
-                    # if measurement % 10 == 0:
-                    self.laser_stabilizer.run()
-                    delay(1 * ms)
-                    self.dds_FORT.sw.on()
-                    self.dds_FORT.set(frequency=self.f_FORT - 30 * MHz, amplitude=self.stabilizer_FORT.amplitude)
-
-                load_MOT_and_FORT(self)
-
-                # set the cooling DP AOM to the readout settings
-                self.dds_cooling_DP.set(frequency=self.f_cooling_DP_RO, amplitude=self.ampl_cooling_DP_MOT)
-
-                if not self.no_first_shot:
-                    # take the first shot
-                    self.dds_cooling_DP.sw.on()
-                    t_gate_end = self.ttl0.gate_rising(self.t_SPCM_first_shot)
-                    counts = self.ttl0.count(t_gate_end)
-                    delay(1*ms)
-                    self.dds_cooling_DP.sw.off()
-
-                # turn the FORT off
-                self.dds_FORT.set(frequency=self.f_FORT - 30 * MHz, amplitude=self.stabilizer_FORT.amplitude)
-
-                delay(t_delay_between_shots)
-
-                # turn the FORT on
-                self.dds_FORT.set(frequency=self.f_FORT, amplitude=self.stabilizer_FORT.amplitude)
-                delay(1*ms)
-
-                # take the second shot
-                self.dds_cooling_DP.sw.on()
-                t_gate_end = self.ttl0.gate_rising(self.t_SPCM_second_shot)
-                counts2 = self.ttl0.count(t_gate_end)
-                delay(1*ms)
-                self.dds_cooling_DP.sw.off()
-
-                self.dds_cooling_DP.set(frequency=self.f_cooling_DP_MOT, amplitude=self.ampl_cooling_DP_MOT)
-
-                delay(2*ms)
-
-                iteration += 1
-
-                # update the datasets
-                if not self.no_first_shot:
-                    self.append_to_dataset('photocounts', counts)
-                    self.append_to_dataset('photocounts_current_iteration', counts)
-
-                # update the datasets
-                self.append_to_dataset('photocounts2', counts2)
-                self.append_to_dataset('photocounts2_current_iteration', counts2)
-                self.set_dataset("iteration", iteration, broadcast=True)
+            # for measurement in range(self.n_measurements):
+            #
+            #     if self.enable_laser_feedback:
+            #         # if measurement % 10 == 0:
+            #         self.laser_stabilizer.run()
+            #         delay(1 * ms)
+            #         self.dds_FORT.sw.on()
+            #         self.dds_FORT.set(frequency=self.f_FORT - 30 * MHz, amplitude=self.stabilizer_FORT.amplitude)
+            #
+            #     load_MOT_and_FORT(self)
+            #
+            #     # set the cooling DP AOM to the readout settings
+            #     self.dds_cooling_DP.set(frequency=self.f_cooling_DP_RO, amplitude=self.ampl_cooling_DP_MOT)
+            #
+            #     if not self.no_first_shot:
+            #         # take the first shot
+            #         self.dds_cooling_DP.sw.on()
+            #         t_gate_end = self.ttl0.gate_rising(self.t_SPCM_first_shot)
+            #         counts = self.ttl0.count(t_gate_end)
+            #         delay(1*ms)
+            #         self.dds_cooling_DP.sw.off()
+            #
+            #     # turn the FORT off
+            #     self.dds_FORT.set(frequency=self.f_FORT - 30 * MHz, amplitude=self.stabilizer_FORT.amplitude)
+            #
+            #     delay(t_FORT_drop)
+            #
+            #     # turn the FORT on
+            #     self.dds_FORT.set(frequency=self.f_FORT, amplitude=self.stabilizer_FORT.amplitude)
+            #     delay(1*ms)
+            #
+            #     # take the second shot
+            #     self.dds_cooling_DP.sw.on()
+            #     t_gate_end = self.ttl0.gate_rising(self.t_SPCM_second_shot)
+            #     counts2 = self.ttl0.count(t_gate_end)
+            #     delay(1*ms)
+            #     self.dds_cooling_DP.sw.off()
+            #
+            #     self.dds_cooling_DP.set(frequency=self.f_cooling_DP_MOT, amplitude=self.ampl_cooling_DP_MOT)
+            #
+            #     delay(2*ms)
+            #
+            #     iteration += 1
+            #
+            #     # update the datasets
+            #     if not self.no_first_shot:
+            #         self.append_to_dataset('photocounts', counts)
+            #         self.append_to_dataset('photocounts_current_iteration', counts)
+            #
+            #     # update the datasets
+            #     self.append_to_dataset('photocounts2', counts2)
+            #     self.append_to_dataset('photocounts2_current_iteration', counts2)
+            #     self.set_dataset("iteration", iteration, broadcast=True)
 
         delay(1*ms)
         # leave MOT on at end of experiment, but turn off the FORT
