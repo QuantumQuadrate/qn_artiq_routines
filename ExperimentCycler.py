683902914dcbe9aace7eef3df0fdce926e336faf
prhuft <gothrowayo@gmail.com>
2024-06-14 08:04:19 -0500

    don't initialize feedback channels in ExperimentCycler. this is done in BaseExperiment

--- ExperimentCycler.py
+++ ExperimentCycler.py
@@ -76,10 +76,6 @@ class ExperimentCycler(EnvExperiment):
         self.set_dataset("photocount_bins", [50], broadcast=True)
         self.set_dataset("iteration", 0, broadcast=True)
 
-        self.set_dataset("feedbackchannels",
-                         [ch.dB_dataset for ch in self.laser_stabilizer.all_channels],
-                         broadcast=True, persist=True)
-
         value = 0.0
         for ch_i in range(len(self.laser_stabilizer.all_channels)):
             self.set_dataset(self.laser_stabilizer.all_channels[ch_i].dB_history_dataset,
