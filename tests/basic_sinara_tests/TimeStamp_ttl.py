7466d9fee8852ba38edac5691957843d487dc930
Akbar <Akbar.Safari@gmail.com>
2025-02-05 11:36:03 -0600

    Revert "11:33"
    This reverts commit 81918a7bbe40e00813f0a0ce40a2aca3c51daef9.

--- tests/basic_sinara_tests/TimeStamp_ttl.py
+++ tests/basic_sinara_tests/TimeStamp_ttl.py
@@ -1,22 +1,27 @@
-### 11:31
+### Test test2
 
-aasda
-sda
-sda
-sd
-a
-sda
-sd
-
-fs
-dgs
-dgs
-dg
-s
-s
-dsdg
-
-
+# from artiq.experiment import *
+# # import pandas as pd
+# import time
+# from artiq.language.core import *
+# from artiq.language.types import *
+#
+#
+# St_t = time.time()
+# file0 = open('C:/Users/QC/OneDrive - UW-Madison/Desktop/Akbar/AkSTimeList0.txt', 'a')
+# file1 = open('C:/Users/QC/OneDrive - UW-Madison/Desktop/Akbar/AkSTimeList1.txt', 'a')
+# class TimeStamp_ttl(EnvExperiment):
+#
+#     def build(self):
+#         self.setattr_device("core")
+#         self.setattr_device("ttl0")
+#         self.setattr_device("ttl1")
+#
+#     @rpc(flags={"async"})
+#     def saveTime0(self, x):
+#         file0.write(str(x) + '\n')
+#         # print(x)
+#
 #     @rpc(flags={"async"})
 #     def saveTime1(self, x):
 #         file1.write(str(x) + '\n')
