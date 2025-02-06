b70353d8610fc8eec2c919aebfa5aea341c3d282
Akbar <Akbar.Safari@gmail.com>
2025-02-06 17:17:45 -0600

    TimeStamp_ttl.py works well to timetag and count ttl0 signals.

--- tests/basic_sinara_tests/TimeStamp_ttl.py
+++ tests/basic_sinara_tests/TimeStamp_ttl.py
@@ -1,112 +1,81 @@
-# from artiq.experiment import *
-# # import pandas as pd
-# import time
-# from artiq.language.core import *
-# from artiq.language.types import *
-#
-#
-# St_t = time.time()
-# file0 = open('C:/Users/QC/OneDrive - UW-Madison/Desktop/Akbar/AkSTimeList0.txt', 'a')
-# file1 = open('C:/Users/QC/OneDrive - UW-Madison/Desktop/Akbar/AkSTimeList1.txt', 'a')
-# class TimeStamp_ttl(EnvExperiment):
-#
-#     def build(self):
-#         self.setattr_device("core")
-#         self.setattr_device("ttl0")
-#         self.setattr_device("ttl1")
-#
-#     @rpc(flags={"async"})
-#     def saveTime0(self, x):
-#         file0.write(str(x) + '\n')
-#         # print(x)
-#
-#     @rpc(flags={"async"})
-#     def saveTime1(self, x):
-#         file1.write(str(x) + '\n')
-#         # print(x)
-#
-#     @kernel
-#     def run(self):
-#         self.core.reset()
-#         self.ttl0.input()
-#         self.ttl1.input()
-#         timestamps = np.range(100)
-#
-#         delay(1 * us)
-#
-#         for x in range(10):
-#             delay(10 * ms)
-#             with parallel:
-#                 tend0 = self.ttl0.gate_rising(10 * us)
-#                 tend1 = self.ttl1.gate_rising(10 * us)
-#
-#             # ### this saves the timestamp of the first event only:
-#             # timePuls0 = self.ttl0.timestamp_mu(tend0)
-#             # timePuls1 = self.ttl1.timestamp_mu(tend1)
-#             # self.saveTime0(self.core.mu_to_seconds(timePuls0))
-#             # self.saveTime1(self.core.mu_to_seconds(timePuls1))
-#             # delay(1 * ms)
-#
-#
-#
-#         self.saveTime0(timestamps)
-#         file0.close()
-#         # file1.close()
-#
-#
-#     def analyze(self):
-#         End_t = time.time()
-#         # self.saveTime1(TimeList)
-#         print("$$$ %s seconds $$$" % round((End_t - St_t), 3))
-#
-#
+"""
+An independent code to monitor ttl0 for signals timestamp the signals and count the total number of time stamps.
+Saves the timestamps as well as the total counts in txt file next to the HDF5 file. Also counts the signals using
+ttl.count and edge counter for comparison.
 
+When timestamp after openning the gate, artiq stamps only one event (I think the first event). To time stamp all the
+events received within  the window I have used a while loop to tag up to max_clicks counts in each window.
 
+timestamping and ttl.count both consume the window, thus, we cannot do ttl.count and timestamp with the same window.
 
+"""
+### time tagging and counting the tags:
 from artiq.experiment import *
+import numpy as np  # Use numpy for random number generation
 
 class TimeStamp_ttl(EnvExperiment):
     def build(self):
         self.setattr_device("core")
         self.setattr_device("ttl0")
+        self.setattr_device("ttl0_counter")
+
+        self.setattr_argument("num_iterations", NumberValue(100, ndecimals=0, step=1))
+        self.setattr_argument("t_SPCM_exposure", NumberValue(100 * us, unit='us'))
 
     @kernel
     def run(self):
         self.core.reset()
         self.ttl0.input()
 
-        num_iterations = 100
-        max_clicks = 20  # Maximum pulses to time-tag per iteration
-        timestamps = [[-1.0] * max_clicks for _ in range(num_iterations)]  # Preallocated array
-        counts = [0] * num_iterations  # Store click counts separately
+        max_clicks = 10 ### maximum number of clicks that will be time tagged in each gate window
+        timestamps = [[-1.0] * max_clicks for _ in range(self.num_iterations)]
+        ttl_counts = [0] * self.num_iterations ### counts we measure using ttl.count
+        edge_counts = [0] * self.num_iterations ### counts we measure using edge counter
+        stamp_counts = [0] * self.num_iterations ### counts we measure by adding up the timestamps
 
-        for i in range(num_iterations):
-            delay(random()*10*us)
-            t_end_SPCM0 = self.ttl0.gate_rising(100 * us)
+        for i in range(self.num_iterations):
+            delay(10*ms)
+            t_end_SPCM0 = self.ttl0.gate_rising(self.t_SPCM_exposure)
 
             click_counter = 0
             while click_counter < max_clicks:
                 click_time = self.ttl0.timestamp_mu(t_end_SPCM0)
-                if click_time == -1:  # No more clicks detected
+                if click_time == -1:
                     break
                 timestamps[i][click_counter] = self.core.mu_to_seconds(click_time)
                 click_counter += 1
 
-            counts[i] = click_counter  # Store the number of detected events
+            ### counting by adding up the timestamps
+            stamp_counts[i] = click_counter
 
-        # Save data outside the kernel
+            ### counting using ttl.count:
+            delay(10 * us)
+            t_end_SPCM0 = self.ttl0.gate_rising(self.t_SPCM_exposure)
+            ttl_counts[i] = self.ttl0.count(t_end_SPCM0)
+
+            ### counting using edge counter:
+            delay(10 * us)
+            self.ttl0_counter.gate_rising(self.t_SPCM_exposure)
+            edge_counts[i] = self.ttl0_counter.fetch_count()
+
+
         self.save_data(timestamps)
-        self.save_counts(counts)
+        self.save_counts("ttl_counts", ttl_counts)
+        self.save_counts("edge_counts", edge_counts)
+        self.save_counts("stamp_counts", stamp_counts)
+
         print("************  ALL DONE  ************")
+
 
     @rpc(flags={"async"})
     def save_data(self, data):
-        with open("AkStimestamps.txt", "a") as file:
+        with open("Click_timestamps.txt", "a") as file:
             for iteration in data:
                 file.write(", ".join(map(str, iteration)) + "\n")
 
     @rpc(flags={"async"})
-    def save_counts(self, data):
-        with open("AkScounts.txt", "a") as file:
+    def save_counts(self, title, data):
+        with open("Click_Counts.txt", "a") as file:
+            file.write(title + ": ")
             file.write(", ".join(map(str, data)) + "\n")
 
