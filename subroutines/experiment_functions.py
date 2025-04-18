e893a9500094ed8416563b0ef7f71b497c8dca69
Akbar <Akbar.Safari@gmail.com>
2025-04-18 15:35:04 -0500

    1- D1_lock is added to end_measurement properly in a while loop. 2- Some files in test folder renamed for organization.

--- subroutines/experiment_functions.py
+++ subroutines/experiment_functions.py
@@ -270,6 +270,7 @@ def load_MOT_and_FORT_until_atom(self):
 
     delay(1 * ms)
     # self.ttl_UV.pulse(self.t_UV_pulse)
+    zotino_stability_test(self)  ### Record one Zotino output just to test its stability.
 
     max_tries = 100  ### Maximum number of attempts before running the feedback
     atom_check_time = 20 * ms
@@ -358,6 +359,153 @@ def load_MOT_and_FORT_until_atom(self):
     ### saving the atom loading time for each loaded atom.
     self.append_to_dataset("Atom_loading_time", self.core.mu_to_seconds(t_after_atom - t_before_atom))
     delay(10 * ms)
+
+@kernel
+def load_MOT_and_FORT_until_atom_recycle(self):
+    """
+    Before attempting to load MOT and FORT, it checks if there is already an atom in the FORT. If not, then it turns on the MOT and FORT
+    light at the same time and monitor SPCM0. Turn off the MOT as soon as an atom is trapped.
+
+    Turns ON the following at the beginning:
+        FORT AOM
+        Cooling DP
+        All fiber AOMs
+        MOT RP
+
+    Leaves the following OFF at the end:
+        Cooling DP
+        MOT RP
+
+    :param self: the experiment instance
+    :return:
+    """
+
+    ### First check if there is already an atom in the FORT based on RO2
+    delay(100 * us)
+    if self.measurement > 0:
+        if self.BothSPCMs_RO2/self.t_SPCM_second_shot > self.single_atom_threshold:
+            atom_loaded = True
+        else:
+            atom_loaded = False
+    else:
+        atom_loaded = False
+
+
+    ### load an atom if atom_loaded = False
+    if not atom_loaded:
+        ### Set the coils to MOT loading setting
+        self.zotino0.set_dac(
+            [self.AZ_bottom_volts_MOT, self.AZ_top_volts_MOT, self.AX_volts_MOT, self.AY_volts_MOT],
+            channels=self.coil_channels)
+
+        ### set the cooling DP AOM to the MOT settings
+        self.dds_cooling_DP.set(frequency=self.f_cooling_DP_MOT, amplitude=self.ampl_cooling_DP_MOT)
+        delay(0.1 * ms)
+
+        self.dds_cooling_DP.sw.on()  ### turn on cooling
+        self.ttl_repump_switch.off()  ### turn on MOT RP
+
+        self.dds_AOM_A1.sw.on()
+        self.dds_AOM_A2.sw.on()
+        self.dds_AOM_A3.sw.on()
+        self.dds_AOM_A4.sw.on()
+        delay(0.1 * ms)
+        self.dds_AOM_A5.sw.on()
+        self.dds_AOM_A6.sw.on()
+
+        self.dds_FORT.set(frequency=self.f_FORT, amplitude=self.stabilizer_FORT.amplitude)
+        self.dds_FORT.sw.on()
+
+        delay(1 * ms)
+        # self.ttl_UV.pulse(self.t_UV_pulse)
+
+        max_tries = 100  ### Maximum number of attempts before running the feedback
+        atom_check_time = 20 * ms
+        atom_loaded = False
+        try_n = 0
+        t_before_atom = now_mu() ### is used to calculate the loading time of atoms by atom_loading_time = t_after_atom - t_before_atom
+        t_after_atom = now_mu()
+        time_without_atom = 0.0
+
+        while True:
+            while not atom_loaded and try_n < max_tries:
+                delay(100 * us)  ### Needs a delay of about 100us or maybe less
+                with parallel:
+                    self.ttl_SPCM0_counter.gate_rising(atom_check_time)
+                    self.ttl_SPCM1_counter.gate_rising(atom_check_time)
+
+                BothSPCMs_atom_check = int((self.ttl_SPCM0_counter.fetch_count() + self.ttl_SPCM1_counter.fetch_count()) / 2)
+
+                try_n += 1
+
+                if BothSPCMs_atom_check / atom_check_time > self.single_atom_threshold:
+                    delay(100 * us)  ### Needs a delay of about 100us or maybe less
+                    atom_loaded = True
+
+            if atom_loaded:
+                # t_before_atom = t_after_atom ### I don't know why I had this! Removed and seems working fine.
+                self.set_dataset("time_without_atom", 0.0, broadcast=True) ### resetting time_without_atom when we load an atom
+                t_after_atom = now_mu()
+                break  ### Exit the outer loop if an atom is loaded
+
+            #### time_without_atom shows how long is passed from the previous atom loading. Calculated only when try_n > max_tries
+            delay(0.1 * ms)
+            t_no_atom = now_mu()
+            time_without_atom = self.core.mu_to_seconds(t_no_atom - t_before_atom)
+            self.set_dataset("time_without_atom", time_without_atom, broadcast=True)
+
+            ### If max_tries reached and still no atom, run feedback
+            if self.enable_laser_feedback:
+                delay(0.1 * ms) ### necessary to avoid underflow
+
+                # delay(0.1 * ms)
+                # self.ttl7.pulse(100 * us)  ### for triggering oscilloscope
+                # delay(0.1 * ms)
+
+                ### todo: set cooling_DP frequency to MOT loading in the stabilizer.
+                ### set the cooling DP AOM to the MOT settings. Otherwise, DP might be at f_cooling_Ro setting during feedback.
+                self.dds_cooling_DP.set(frequency=self.f_cooling_DP_MOT, amplitude=self.ampl_cooling_DP_MOT)
+                delay(0.1 * ms)
+                self.laser_stabilizer.run()
+                # bug -- microwave dds and FORT are off after AOM feedback; not clear why yet. for now, just turn them back on
+                self.dds_microwaves.sw.on()
+                self.dds_FORT.sw.on()
+                delay(0.1 * ms)
+
+                try_n = 0
+
+                # delay(10 * ms)
+                # print("**************   No atom after 2 seconds. Running feedback   ***************")
+                # delay(10 * ms)
+
+        ### Set the coils to PGC setting even when we don't want PGC. Effectively, this is turning off coils.
+        self.zotino0.set_dac(
+            [self.AZ_bottom_volts_PGC, self.AZ_top_volts_PGC, self.AX_volts_PGC, self.AY_volts_PGC],
+            channels=self.coil_channels)
+        delay(0.4 * ms)
+
+        ###########  PGC on the trapped atom  #############
+
+        ### set the cooling DP AOM to the PGC settings
+        self.dds_cooling_DP.set(frequency=self.f_cooling_DP_PGC, amplitude=self.ampl_cooling_DP_PGC)
+        delay(20 * ms) ### this is the PGC time
+        ###################################################
+
+        self.ttl_repump_switch.on()  ### turn off MOT RP
+        self.dds_cooling_DP.sw.off()  ### turn off cooling
+
+        delay(1 * ms)
+        self.stabilizer_FORT.run(setpoint_index=1)  # the science setpoint
+        delay(self.t_MOT_dissipation)  # should wait several ms for the MOT to dissipate
+
+        ### I don't know what this SPCM0_FORT_science is used for. Set to 0 for now:
+        self.SPCM0_FORT_science = 0
+        # t_gate_end = self.ttl_SPCM0.gate_rising(self.t_SPCM_first_shot)
+        # self.SPCM0_FORT_science = self.ttl_SPCM0.count(t_gate_end)
+
+        ### saving the atom loading time for each loaded atom.
+        self.append_to_dataset("Atom_loading_time", self.core.mu_to_seconds(t_after_atom - t_before_atom))
+        delay(10 * ms)
 
 @kernel
 def load_MOT_and_FORT_for_Luca_scattering_measurement(self):
@@ -1526,14 +1674,25 @@ def end_measurement(self):
     if self.__class__.__name__ != 'ExperimentCycler':
         if self.require_atom_loading_to_advance:
             if not self.SPCM0_RO1/self.t_SPCM_first_shot > self.single_atom_threshold:
-                advance *= 0
+                advance = 0
+
         if self.require_D1_lock_to_advance:
-            self.ttl_D1_lock_monitor.sample_input()
-            delay(0.1 * ms)
-            laser_locked = int(1 - self.ttl_D1_lock_monitor.sample_get())
-            advance *= laser_locked
-            if not laser_locked:
-                logging.warning("D1 laser not locked")
+            t1_D1_checked = now_mu()
+            while True:
+                self.ttl_D1_lock_monitor.sample_input()
+                delay(0.1 * ms)
+                laser_locked = int(1 - self.ttl_D1_lock_monitor.sample_get()) ## this is 1 when the laser is locked, it is 0 otherwise.
+
+                if laser_locked:
+                    advance = 1
+                    self.set_dataset("time_without_D1", 0.0, broadcast=True)  ### resetting time_without_D1 when the laser is locked
+                    delay(10*us)
+                    break
+                else:
+                    t2_D1_checked = now_mu()
+                    time_without_D1 = self.core.mu_to_seconds(t2_D1_checked - t1_D1_checked)
+                    self.set_dataset("time_without_D1", time_without_D1, broadcast=True)
+                    delay(2 * s)
 
     if advance:
         self.measurement += 1
@@ -1701,7 +1860,7 @@ def atom_loading_2_experiment(self):
 
     # self.print_async("in exp:",self.AZ_bottom_volts_MOT, self.AZ_top_volts_MOT, self.AX_volts_MOT, self.AY_volts_MOT)
 
-    self.require_D1_lock_to_advance = False # override experiment variable
+    # self.require_D1_lock_to_advance = True # override experiment variable
 
     if self.enable_laser_feedback:
         self.laser_stabilizer.run()
@@ -2010,14 +2169,10 @@ def microwave_Rabi_2_experiment(self):
         ############################
 
         if self.t_microwave_pulse > 0.0:
-            # self.ttl_repump_switch.on()  # turns off the RP AOM
-
-            # todo: set coils for microwaves. good for diagnostics-- we can use this phase to zero the B-field
-            self.zotino0.set_dac(
-                [self.AZ_bottom_volts_OP, self.AZ_top_volts_OP,
-                 self.AX_volts_OP, self.AY_volts_OP],
-                channels=self.coil_channels)
-            delay(0.3*ms)
+            # ### Changing the bias field for testing.
+            # self.zotino0.set_dac([self.AZ_bottom_volts_microwave, self.AZ_top_volts_microwave,self.AX_volts_microwave, self.AY_volts_microwave],
+            #                      channels=self.coil_channels)
+            delay(0.5*ms)
 
             # self.ttl7.pulse(self.t_exp_trigger)  # in case we want to look at signals on an oscilloscope
 
@@ -2228,6 +2383,7 @@ def microwave_freq_scan_with_photons_experiment(self):
         self.ttl_exc0_switch.on()  # turns off the excitation
 
         load_MOT_and_FORT_until_atom(self)
+        # load_MOT_and_FORT_until_atom_recycle(self)
         delay(0.1 * ms)
 
         first_shot(self)
@@ -2306,6 +2462,8 @@ def microwave_freq_scan_with_photons_experiment(self):
         self.GRIN1and2_dds.set(frequency=self.f_excitation, amplitude=dB_to_V(self.p_excitation))
 
         ### Change the bias field during excitation and detection
+        # self.AZ_top_volts_microwave = - self.AZ_bottom_volts_microwave ### To use the quad coils as shims.
+        # self.AZ_top_volts_microwave = 0.174
         self.zotino0.set_dac(
             [self.AZ_bottom_volts_microwave, self.AZ_top_volts_microwave, self.AX_volts_microwave, self.AY_volts_microwave],
             channels=self.coil_channels)
@@ -2368,13 +2526,11 @@ def microwave_freq_scan_with_photons_experiment(self):
         self.dds_AOM_A5.sw.off()
         self.dds_AOM_A6.sw.off()
 
-        delay(1*ms)
+        end_measurement(self)
+        delay(6 * ms) ### hopefully to avoid underflow.
         self.append_to_dataset('SPCM0_SinglePhoton', SPCM0_SinglePhoton)
         self.append_to_dataset('SPCM1_SinglePhoton', SPCM1_SinglePhoton)
         delay(1 * ms)
-
-        end_measurement(self)
-        delay(5 * ms) ### hopefully to avoid underflow.
 
     delay(10*ms)
     self.dds_FORT.sw.off()
@@ -3449,7 +3605,7 @@ def single_photon_experiment_3_atom_loading_advance(self):
         SPCM0_timestamps = [[-1.0] * max_clicks for _ in range(self.max_excitation_cycles * self.n_excitation_attempts)]
         SPCM1_timestamps = [[-1.0] * max_clicks for _ in range(self.max_excitation_cycles * self.n_excitation_attempts)]
 
-        delay(40 * ms) ### with n_excitation_attempts = 5, 30ms delay is not enough
+        delay(100 * ms) ### with n_excitation_attempts = 5, 30ms delay is not enough
 
         self.ttl_exc0_switch.on()  # turns off the excitation
         delay(1 * ms)
@@ -3545,9 +3701,9 @@ def single_photon_experiment_3_atom_loading_advance(self):
             # self.GRIN1and2_dds.set(frequency=self.f_excitation, amplitude=self.stabilizer_excitation.amplitudes[0])
             self.GRIN1and2_dds.set(frequency=self.f_excitation, amplitude=dB_to_V(self.p_excitation))
 
-            # ### zero the bias field for testing only
+            # ### Changing the bias field to test the effect of Zeeman shift on the photons
             # self.zotino0.set_dac(
-            #     [self.AZ_bottom_volts_PGC, self.AZ_top_volts_PGC, self.AX_volts_PGC, self.AY_volts_PGC],
+            #     [self.AZ_bottom_volts_microwave, self.AZ_top_volts_microwave, self.AX_volts_microwave, self.AY_volts_microwave],
             #     channels=self.coil_channels)
 
             self.ttl_exc0_switch.off() # turns on the excitation0 AOM
@@ -3654,8 +3810,6 @@ def single_photon_experiment_3_atom_loading_advance(self):
                 self.dds_AOM_A5.sw.on()
                 self.dds_AOM_A6.sw.on()
 
-
-
                 with parallel:
                     self.ttl_SPCM0_counter.gate_rising(self.t_SPCM_recool_and_shot)
                     self.ttl_SPCM1_counter.gate_rising(self.t_SPCM_recool_and_shot)
@@ -3664,14 +3818,6 @@ def single_photon_experiment_3_atom_loading_advance(self):
                 SPCM1_RO_atom_check = self.ttl_SPCM1_counter.fetch_count()
                 BothSPCMs_RO_atom_check = int((SPCM0_RO_atom_check + SPCM1_RO_atom_check) / 2)
                 BothSPCMs_RO_atom_check_array[int(excitation_cycle / self.atom_check_every_n)] = BothSPCMs_RO_atom_check
-
-
-
-                # self.ttl_SPCM0_counter.gate_rising(self.t_SPCM_recool_and_shot)
-                # SPCM0_RO_atom_check = self.ttl_SPCM0_counter.fetch_count()
-                # SPCM0_RO_atom_check_array[int(excitation_cycle/self.atom_check_every_n)] = SPCM0_RO_atom_check
-
-
 
                 ### stopping the excitation cycle after the atom is lost
                 if BothSPCMs_RO_atom_check / self.t_SPCM_recool_and_shot < self.single_atom_threshold:
@@ -3707,7 +3853,7 @@ def single_photon_experiment_3_atom_loading_advance(self):
 
         end_measurement(self)
 
-        delay(1 * ms)
+        delay(5 * ms)
 
         ### only the elements in range [0:excitation_cycle + 1] contain non-zero values because the loop exits after
         ### the atom is lost. +1 is because python sttops the loop one count earlier.
