2db670ff423d6b29bdbc4e5f0338ee455ff2aa9c
Akbar <Akbar.Safari@gmail.com>
2025-11-25 15:33:12 -0600

    A12-Testing_chopped_RO_with_sensitivity.py works fine for chopped readout using sensitivity on DMA.

--- tests/A12-Testing_chopped_RO_with_sensitivity.py
+++ tests/A12-Testing_chopped_RO_with_sensitivity.py
@@ -1,8 +1,7 @@
 """
 Testing the chopped RO using self.ttl0._set_sensitivity(0) and (1) in DMA.
 Here, I am chopping the FORT and change the ttl sensitivity both on DMA. I am simply measuring the Raman noise from the FORT. The code below
-works well and I can do RO when the FORT is off or when the FORT is ON by changing t_RO_delay from 0ns to 900ns. I have tried cycles up to
-4000 which takes about 7ms in total.
+works well and I can do RO when the FORT is off or when the FORT is ON. I have tried cycles up to 4000 which takes about 7ms in total.
 
 Akbar 2025-11-24
 
@@ -33,33 +32,36 @@ class Testing_chopped_RO_with_sensitivity(EnvExperiment):
 
     @kernel
     def record_chopped_FORT_and_TTL0(self):
-        t_FORT_ON = self.core.seconds_to_mu(1*us) ### the length of the FORT pulse
-        t_cooling_delay = self.core.seconds_to_mu(100*ns) ### delay wrt "t0" in each cycle
-        t_RO = self.core.seconds_to_mu(0.8*us) ### duration of RO
-        t_FORT_delay = self.core.seconds_to_mu(100*ns) ### delay wrt "t0" in each cycle
-        t_RO_delay = self.core.seconds_to_mu(900*ns)
+        t_FORT_offset = self.core.seconds_to_mu(100 * ns)  ### delay wrt "t0" in each cycle
+        t_FORT_OFF = self.core.seconds_to_mu(self.chop_RO_FORT_off) ### the length of the FORT OFF time
+
+        t_cooling_offset = self.core.seconds_to_mu(self.chop_RO_pulse_offset) ### delay wrt "t0" in each cycle
+        t_cooling_length = self.core.seconds_to_mu(self.chop_RO_pulse_length) ### duration of the cooling pulse
+
+        t_gate_offset = self.core.seconds_to_mu(self.chop_RO_gate_offset) ### delay wrt t0 for gate to open
+        t_gate_length = self.core.seconds_to_mu(self.chop_RO_gate_length) ### gate open time
 
         with self.core_dma.record("FORT_and_TTL0_chopped"):
             for i in range(4000): ### number of cycles to repeat
                 t0 = now_mu()
 
-                at_mu(t0+t_FORT_delay)
-                self.dds_FORT.sw.on()
-                at_mu(t0 + t_FORT_delay + t_FORT_ON)
+                at_mu(t0 + t_FORT_offset)
                 self.dds_FORT.sw.off()
+                at_mu(t0 + t_FORT_offset + t_FORT_OFF)
+                self.dds_FORT.sw.on()
 
-                # at_mu(t0 + t_cooling_delay)
-                # self.dds_cooling_DP.sw.on()  ### Turn on cooling
-                # at_mu(t0 + t_cooling_delay + t_RO)
-                # self.dds_cooling_DP.sw.off()  ### Turn off cooling
+                at_mu(t0 + t_cooling_offset)
+                self.dds_cooling_DP.sw.on()  ### Turn on cooling
+                at_mu(t0 + t_cooling_offset + t_cooling_length)
+                self.dds_cooling_DP.sw.off()  ### Turn off cooling
 
-                at_mu(t0 + t_RO_delay)
-                self.ttl_GRIN1_switch.on() ### GRIN1_switch is simply used with set_sensitivity to represent that on scope.
+                at_mu(t0 + t_gate_offset)
+                # self.ttl_GRIN1_switch.on() ### GRIN1_switch is simply used with set_sensitivity to represent that on scope.
                 self.ttl0._set_sensitivity(1)
                 self.ttl1._set_sensitivity(1)
 
-                at_mu(t0 + t_RO_delay + t_RO)
-                self.ttl_GRIN1_switch.off()
+                at_mu(t0 + t_gate_offset + t_gate_length)
+                # self.ttl_GRIN1_switch.off()
                 self.ttl0._set_sensitivity(0)
                 self.ttl1._set_sensitivity(0)
 
@@ -70,8 +72,19 @@ class Testing_chopped_RO_with_sensitivity(EnvExperiment):
         self.core.reset()
         self.base.initialize_hardware()
 
+        self.dds_cooling_DP.sw.off()  ### Turn off cooling
+
         delay(1 * ms)
-        self.dds_FORT.sw.off()
+        self.dds_FORT.sw.on()
+        self.dds_AOM_A1.sw.on()
+        self.dds_AOM_A2.sw.on()
+        self.dds_AOM_A3.sw.on()
+        self.dds_AOM_A4.sw.on()
+        delay(1 * ms)
+        if not self.PGC_and_RO_with_on_chip_beams:
+            self.dds_AOM_A5.sw.on()
+            self.dds_AOM_A6.sw.on()
+
         delay(1 * ms)
         self.ttl0._set_sensitivity(0)
         self.ttl_GRIN1_switch.off()
