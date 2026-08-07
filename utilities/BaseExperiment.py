667183ce4fa3f357dc2e3cea59f35341bb7eb662
Eunji Oh <eunji.oh@wisc.edu>
2026-08-07 09:54:59 -0500

    Adding dataset

--- utilities/BaseExperiment.py
+++ utilities/BaseExperiment.py
@@ -970,6 +970,8 @@ class BaseExperiment:
         ### Used in single photon experiments with indivdual nodes
         self.experiment.set_dataset("SPCM0_SinglePhoton_tStamps", [[0.0,0.0]], broadcast=True)
         self.experiment.set_dataset("SPCM1_SinglePhoton_tStamps", [[0.0,0.0]], broadcast=True)
+        self.experiment.set_dataset("SPCM0_OtherNode_SinglePhoton_tStamps", [[0.0,0.0]], broadcast=True)
+        self.experiment.set_dataset("SPCM1_OtherNode_SinglePhoton_tStamps", [[0.0,0.0]], broadcast=True)
 
         ### Added from 2026-06-09 for two-node experiments
         self.experiment.set_dataset("SPCM0_Photon_tStamps", [[0.0,0.0]], broadcast=True)
